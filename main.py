b1001cd427951af19db6aa0585b36759ca2c38fa
sadal <leshasadretdinov@gmail.com>
2025-11-20 17:10:14 +0300

    добавил импорт

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from PyQt6.QtGui import QPainter, QColor, QPolygonF
 from PyQt6.QtCore import Qt, QRectF, QPointF
 from PyQt6.QtWidgets import QWidget, QApplication
 from random import randint
-from math import sin, cos, pi
-
+from PyQt6 import uic
 
 
 class Suprematism(QWidget):
     def __init__(self):
         super().__init__()
-        self.initUI()
+        uic.loadUi('Ui.ui', self)  # Загружаем дизайн
+        self.pushButton.clicked.connect(self.run)
         self.do_paint = False
         self.x, self.y = -100, -100
-        self.fig = 3
+        self.fig = 0
         self.setMouseTracking(True)
+
